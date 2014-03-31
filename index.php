72ca441ff4657dd59cf1a115c317b1c3af705e21
serenasimkus <sks2187@columbia.edu>
2014-03-31 15:35:22 -0400

    edited register, index, logout

--- index.php
+++ index.php
@@ -38,11 +38,12 @@ oci_close($conn);
 					</ul>
 					<ul class="nav navbar-nav pull-right">
 						<li><?php
-        						if($_SESSION['User']){
-                					echo("Welcome, ".$_SESSION['User']);
-        						} else {
-                					echo('<a href="login.php">Log In</a>');
-        						}
+        					if($_SESSION['User']){
+                				echo('<li><a href="#">Welcome, ".$_SESSION['User']."</a></li>');
+								echo('<li><a href="logout.php">Log Out</a></li>');
+        					} else {
+                				echo('<li><a href="login.php">Log In</a></li>'');
+        					}
 							?></li>
 					</ul>
 				</div><!--/.nav-collapse -->
--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
 	session_start();
-	unset($_SESSION[‘User’];
+	session_destroy();
 	header('Location: login.php');
 ?>
